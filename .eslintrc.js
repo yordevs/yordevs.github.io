770cfbcd9f595b86b2eb576047e9aed99499c14d
Ben Silverman <bensilverman10@gmail.com>
2021-02-06 18:18:41 +0000

    Remove linebreak eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -15,7 +15,6 @@ module.exports = {
   plugins: ["react"],
   rules: {
     indent: ["error", 2],
-    "linebreak-style": ["error", "unix"],
     quotes: ["error", "double"],
     semi: ["error", "always"],
     "react/display-name": 0,
