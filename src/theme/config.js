cfac0537912533d6efd83172ac99f89a24c09f54
Ben Silverman <bensilverman10@gmail.com>
2021-02-27 19:10:02 +0000

    Add breakpoints for responsiveness

--- src/theme/config.js
+++ src/theme/config.js
@@ -4,6 +4,11 @@ const config = {
     size: { base: "16px" },
     weight: { normal: "400", semibold: "600", bold: "700" },
   },
+  breakpoint: {
+    //sm: "0px" and higher
+    md: "min-width: 540px",
+    lg: "min-width: 1072px",
+  },
 };
 
 export default config;
