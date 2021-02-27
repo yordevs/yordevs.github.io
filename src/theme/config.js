ccca7bcab3a23321baa0211cf5ec2b46c3746939
Ben Silverman <bensilverman10@gmail.com>
2021-02-27 16:13:38 +0000

    Create theme folder

---
+++ src/theme/config.js
@@ -0,0 +1,9 @@
+const config = {
+  colors: { accent: "#FF5715" },
+  fonts: {
+    sizes: { base: "16px" },
+    weight: { normal: "400", semibold: "600", bold: "700" },
+  },
+};
+
+export default config;
--- src/theme/globalStyles.js
+++ src/theme/globalStyles.js
@@ -1,5 +1,9 @@
 import { createGlobalStyle } from "styled-components";
 
+import config from "./config";
+
+const { colors, fonts } = config;
+
 const GlobalStyle = createGlobalStyle`
   /* START OF CSS RESET */
   * {
@@ -11,7 +15,7 @@ const GlobalStyle = createGlobalStyle`
   }
 
   html {
-    font-size: 16px;
+    font-size: ${fonts.sizes.base};
   }
   /* END OF RESET */
 
@@ -20,6 +24,29 @@ const GlobalStyle = createGlobalStyle`
     - font sizes for at least p, h1, h2, h3
     - margin & padding for at least p, h1, h2, h3
   */
+  body {
+    font-family: "Poppins", Arial, Helvetica, sans-serif;
+    font-weight: 300;
+  }
+
+  h1 {font-size:2rem}
+  h2 {font-size:1.5rem}
+  h3 {font-size:1.2rem}
+
+  h1, h2, h3 {
+    margin-bottom: 0.5rem;
+    font-weight: 700;
+  }
+
+  p {
+    margin-bottom: 0.8rem;
+    line-height: 1.3;
+  }
+
+  ::selection {
+    color: white;
+    background: ${colors.accent};
+  }
 `;
 
 export default GlobalStyle;
