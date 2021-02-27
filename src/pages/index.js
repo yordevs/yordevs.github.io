e916fa7972329f7941073461bc4c55649051852a
Ben Silverman <bensilverman10@gmail.com>
2021-02-27 16:14:21 +0000

    Fill page with dummy content

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,6 +8,32 @@ const IndexPage = () => (
     description="Welcome to Yordevs, the University of York's Web Development Society!">
     <h1>Yordevs</h1>
     <p>Hello, world!</p>
+
+    <h1>This is a Heading 1</h1>
+    <h2>This is a Heading 2</h2>
+    <h3>This is a Heading 3</h3>
+    <p>This is a paragraph</p>
+    <p>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut sit amet convallis nulla, ac
+      ullamcorper nunc. Fusce ultrices mi eu lectus gravida iaculis. Phasellus sit amet turpis
+      molestie risus fermentum porttitor. Nulla vel leo vel magna elementum consequat non accumsan
+      tortor. Nulla arcu purus, dictum eu tristique eu, viverra et nulla. Phasellus in dictum risus,
+      a aliquam risus. Maecenas ut lorem in massa dignissim finibus. Aliquam aliquet eget arcu in
+      convallis. Nam feugiat dui vel elementum euismod. Maecenas facilisis eleifend leo, in pulvinar
+      orci fringilla vel. Suspendisse in libero ac ipsum condimentum pellentesque.
+    </p>
+
+    <p>
+      Mauris malesuada hendrerit ex. Cras vulputate quis mauris quis varius. In vitae sodales neque.
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit. Aenean consequat vel lacus eu
+      interdum. Pellentesque finibus metus tellus, quis semper ipsum rutrum quis. Vivamus
+      pellentesque nunc magna, a bibendum risus ullamcorper cursus. Nam commodo libero eu nulla
+      pharetra pretium. Morbi porta purus et semper pretium. Nullam eu turpis sit amet ipsum
+      elementum ullamcorper. Cras in lacus condimentum, luctus felis quis, auctor elit. Donec sit
+      amet massa nec ex bibendum iaculis non ac est. Fusce commodo volutpat mi, eu blandit libero
+      tristique quis. Sed id urna sapien. Aenean convallis nulla id dui rutrum, ac molestie elit
+      ultricies.
+    </p>
   </Layout>
 );
 
