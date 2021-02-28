f53de1d7db5fe73faff1bd46bf51e0829b0ace2d
Ben Silverman <bensilverman10@gmail.com>
2021-02-28 16:26:18 +0000

    Add button component

---
+++ src/components/Button.js
@@ -0,0 +1,43 @@
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
+
+import config from "../theme/config";
+const { color, font } = config;
+
+import { hexToRGBA } from "../theme/util";
+
+const StyledButton = styled(({ as }) => as)`
+  text-decoration: none;
+  display: inline-block;
+  background-color: ${({ variant }) => (variant === "primary" ? color.accent : color.background)};
+  color: ${(props) => (props.variant === "primary" ? color.background : color.accent)};
+
+  border: 2px solid ${color.accent};
+  border-radius: 25rem;
+
+  font-weight: ${font.weight.semibold};
+
+  padding: 0.3em 2em;
+  margin: 0.5em 0;
+
+  width: fit-content;
+
+  transition: all 0.15s ease-in-out;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background-color: ${({ variant }) =>
+      variant === "primary" ? color.background : hexToRGBA(color.accent, 0.3)};
+    color: ${color.accent};
+  }
+`;
+
+const Button = ({ to, href, children, variant }) => (
+  <StyledButton as={to ? Link : "a"} to={to} href={href} variant={variant}>
+    {children}
+  </StyledButton>
+);
+
+export default Button;
