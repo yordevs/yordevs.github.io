833b22e8108d54d8f9084a1e07c01d3b540d3cc4
4dambarr <ab2474@york.ac.uk>
2021-03-07 14:44:43 +0000

    Created text link

---
+++ src/components/TextLink.js
@@ -0,0 +1,36 @@
+/* eslint-disable indent */
+import React from "react";
+import styled from "styled-components";
+import { Link } from "gatsby";
+import PropTypes from "prop-types";
+
+import config from "../theme/config";
+const { color } = config;
+
+const StyledTextLink = styled(({ as }) => as)`
+  text-decoration: none;
+  color: ${color.accent};
+
+  transition: all 0.15s ease-in-out;
+
+  &:hover,
+  &:focus,
+  &:active {
+    background: ${color.accent};
+    color: ${color.background};
+  }
+`;
+
+const TextLink = ({ to, href, children }) => (
+  <StyledTextLink as={to ? Link : "a"} to={to} href={href} target={href ? "_blank" : null}>
+    {children}
+  </StyledTextLink>
+);
+
+export default TextLink;
+
+TextLink.propTypes = {
+  to: PropTypes.string,
+  href: PropTypes.string,
+  children: PropTypes.node,
+};
