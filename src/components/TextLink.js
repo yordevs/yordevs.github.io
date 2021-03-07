534bd3b1d1d56a32814a5e48990ea2e82dd9d550
4dambarr <ab2474@york.ac.uk>
2021-03-07 15:08:34 +0000

    Added suggestions

--- src/components/TextLink.js
+++ src/components/TextLink.js
@@ -5,13 +5,15 @@ import { Link } from "gatsby";
 import PropTypes from "prop-types";
 
 import config from "../theme/config";
-const { color } = config;
+const { color, font } = config;
 
 const StyledTextLink = styled(({ as }) => as)`
   text-decoration: none;
   color: ${color.accent};
 
   transition: all 0.15s ease-in-out;
+
+  font-weight: ${font.weight.semibold};
 
   &:hover,
   &:focus,
@@ -22,7 +24,12 @@ const StyledTextLink = styled(({ as }) => as)`
 `;
 
 const TextLink = ({ to, href, children }) => (
-  <StyledTextLink as={to ? Link : "a"} to={to} href={href} target={href ? "_blank" : null}>
+  <StyledTextLink
+    as={to ? Link : "a"}
+    to={to}
+    href={href}
+    target={href ? "_blank" : null}
+    rel={href ? "noopener" : null}>
     {children}
   </StyledTextLink>
 );
