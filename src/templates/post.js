458706815686b0fdc228e8273377f3adec4db6c0
4dambarr <ab2474@york.ac.uk>
2021-04-17 10:18:59 +0100

    Setup call

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { graphql } from "gatsby";
 import styled from "styled-components";
@@ -14,14 +14,40 @@ const Details = styled.p`
   margin-bottom: 1em;
 `;
 
+const fetchViews = (slug, setViews) => {
+  fetch("http://localhost:4000/new-visit", {
+    method: "POST",
+    mode: "cors",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({ Page: slug }),
+  })
+    .then((response) => {
+      return response.json();
+    })
+    .then((json) => {
+      setViews(json.views);
+    })
+    .catch((err) => {
+      console.log(err);
+      setViews("Error when fetching");
+    });
+};
+
 const PostTemplate = ({
   data: {
     markdownRemark: {
-      frontmatter: { title, date, author },
+      frontmatter: { title, date, author, slug },
       html,
     },
   },
 }) => {
+  const [views, setViews] = useState("Fetching...");
+  useEffect(() => {
+    fetchViews(slug, setViews);
+  }, []);
+
   return (
     <Layout title={title}>
       <article className="blog-post">
@@ -29,6 +55,8 @@ const PostTemplate = ({
         <Title>{title}</Title>
         <Details>
           {author} on {date}
+          <br />
+          Views: {views}
         </Details>
         <div dangerouslySetInnerHTML={{ __html: html }} />
       </article>
