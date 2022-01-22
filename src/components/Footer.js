9f1393f712c233e5042c99d6ed883832874c851b
Harry Wickham <51520004+HarryWickham@users.noreply.github.com>
2022-01-22 11:40:27 +0000

    #73 Created VisuallyHidden component (#99)
    * Created VisuallyHidden component
    added to socialIcons
    
    * Implement sugested changes and merged components together
    
    Co-authored-by: Ben <bensilverman10@gmail.com>

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,6 +1,6 @@
 import React from "react";
 import styled from "styled-components";
-import { FaTwitter, FaInstagram, FaDiscord, FaFacebookF, FaGithub} from "react-icons/fa";
+import { FaTwitter, FaInstagram, FaDiscord, FaFacebookF, FaGithub } from "react-icons/fa";
 
 import SocialIcon from "../components/SocialIcon";
 
@@ -74,11 +74,32 @@ const Footer = () => (
       © {new Date().getFullYear()} York Community Web and App Development
     </CopyrightInfo>
     <SocialLogoHolder>
-      <FooterSocialIcon href="https://github.com/yordevs" icon={<FaGithub />} />
-      <FooterSocialIcon href="https://facebook.com/yordevs" icon={<FaFacebookF />} />
-      <FooterSocialIcon href="https://twitter.com/yordevs" icon={<FaTwitter />} />
-      <FooterSocialIcon href="https://instagram.com/yordevs" icon={<FaInstagram />} />
-      <FooterSocialIcon href="https://discord.gg/j8fbqEP2TD" icon={<FaDiscord />} />
+      <FooterSocialIcon
+        href="https://github.com/yordevs"
+        icon={<FaGithub />}
+        iconName="Link to Yordevs Github"
+      />
+
+      <FooterSocialIcon
+        href="https://facebook.com/yordevs"
+        icon={<FaFacebookF />}
+        iconName="Link to Yordevs Facebook"
+      />
+      <FooterSocialIcon
+        href="https://twitter.com/yordevs"
+        icon={<FaTwitter />}
+        iconName="Link to Yordevs Twitter"
+      />
+      <FooterSocialIcon
+        href="https://instagram.com/yordevs"
+        icon={<FaInstagram />}
+        iconName="Link to Yordevs Instagram"
+      />
+      <FooterSocialIcon
+        href="https://discord.gg/j8fbqEP2TD"
+        icon={<FaDiscord />}
+        iconName="Link to Yordevs Discord"
+      />
     </SocialLogoHolder>
   </StyledFooter>
 );
